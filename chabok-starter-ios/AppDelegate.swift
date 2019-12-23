870e03497606d1e5f975ffd60b39aded6741f135
Farbod <farbod.ps@gmail.com>
2019-12-23 17:02:21 +0330

    add/remove/unset user attribute sample

--- chabok-starter-ios/AppDelegate.swift
+++ chabok-starter-ios/AppDelegate.swift
@@ -10,6 +10,9 @@ import UIKit
 import AdpPushClient
 import CoreData
 import AudioToolbox
+import Firebase
+import OneSignal
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, PushClientManagerDelegate {
@@ -19,10 +22,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PushClientManagerDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        // Chabok initialization
         manager?.addDelegate(self) //Optional
         manager?.logLevel = ChabokLogLevelVerbose //Optional
         
         manager?.configureEnvironment(.Sandbox)
+        
+        // Firebase initialization
+        FirebaseApp.configure()
+        
+        // OneSignal initialization
+        OneSignal.initWithLaunchOptions(launchOptions,
+                                        appId: "9bebc848-343f-47b9-878a-97372c0bb2d1",
+                                        handleNotificationAction: nil,
+                                        settings: onesignalInitSettings)
+        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification;
+        OneSignal.promptForPushNotifications(userResponse: { accepted in
+            print("User accepted notifications: \(accepted)")
+        })
+        
+        // Parse initialization
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "qy8KAy39wq1j4WaZ8UgTRGN0jsp59bUvUAF7dz0a"
+            $0.clientKey = "Dkpj7oQHDwDqAeil9p5YL1jQ8sRY0IG3MBBURms3"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
+        saveInstallationObject()
         
         return true
     }
@@ -69,6 +95,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PushClientManagerDelegate
             print("Error")
         default:
             print("Unknown")
+        }
+    }
+    
+    func saveInstallationObject() {
+        if let installation = PFInstallation.current() {
+            installation.saveInBackground {
+                (success: Bool, error: Error?) in
+                if (success) {
+                    print("You have successfully connected your app to Back4App!")
+                } else {
+                    if let myError = error {
+                        print(myError.localizedDescription)
+                    } else {
+                        print("Uknown error")
+                    }
+                }
+            }
         }
     }
 }
